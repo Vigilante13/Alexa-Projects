5c670949110540bef050249fb043853d0fffbefe
Varun Iyer <varun_iyer@icloud.com>
2018-01-25 17:27:28 +0000

    Added January Project

---
+++ 2018/January/index.js
@@ -0,0 +1,123 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+ * This sample demonstrates a sample skill built with Amazon Alexa Skills nodejs
+ * skill development kit.
+ * This sample supports multiple languages (en-US, en-GB, de-GB).
+ * The Intent Schema, Custom Slot and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-howto
+ **/
+
+'use strict';
+
+const Alexa = require('alexa-sdk');
+const recipes = require('./hero_counters');
+
+const APP_ID = 'amzn1.ask.skill.8ca45dc1-a3fe-4071-9a61-3a04a986e490'; // TODO replace with your app ID (OPTIONAL).
+
+const languageStrings = {
+    'en': {
+        translation: {
+            RECIPES: recipes.RECIPE_EN_US,
+            // TODO: Update these messages to customize.
+            SKILL_NAME: 'Hero Picker',
+            WELCOME_MESSAGE: "Welcome to %s. You can ask a question like, what are counters to Death Prophet ... Now, what can I help you with?",
+            WELCOME_REPROMPT: 'For instructions on what you can say, please say help me.',
+            DISPLAY_CARD_TITLE: '%s  - Counters to %s are ',
+            HELP_MESSAGE: "You can ask questions such as, what are counters, or, you can say exit...Now, what can I help you with?",
+            HELP_REPROMPT: "You can say things like, what are counters, or you can say exit...Now, what can I help you with?",
+            STOP_MESSAGE: 'Goodbye!',
+            RECIPE_REPEAT_MESSAGE: 'Try saying repeat.',
+            RECIPE_NOT_FOUND_MESSAGE: "I\'m sorry, I currently do not know ",
+            RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'the counters for %s. ',
+            RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'counters to that hero. ',
+            RECIPE_NOT_FOUND_REPROMPT: 'What else can I help with?',
+        },
+    },
+};
+
+const handlers = {
+    //Use LaunchRequest, instead of NewSession if you want to use the one-shot model
+    // Alexa, ask [my-skill-invocation-name] to (do something)...
+    'LaunchRequest': function() {
+        this.attributes.speechOutput = this.t('WELCOME_MESSAGE', this.t('SKILL_NAME'));
+        // If the user either does not reply to the welcome message or says something that is not
+        // understood, they will be prompted again with this text.
+        this.attributes.repromptSpeech = this.t('WELCOME_REPROMPT');
+
+        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.emit(':responseReady');
+    },
+    'HeroIntent': function() {
+        const itemSlot = this.event.request.intent.slots.Item;
+        let itemName;
+        if (itemSlot && itemSlot.value) {
+            itemName = itemSlot.value.toLowerCase();
+        }
+
+        const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), itemName);
+        const myRecipes = this.t('RECIPES');
+        const recipe = myRecipes[itemName];
+
+        if (recipe) {
+            this.attributes.speechOutput = recipe;
+            this.attributes.repromptSpeech = this.t('RECIPE_REPEAT_MESSAGE');
+
+            this.response.speak(recipe).listen(this.attributes.repromptSpeech);
+            this.response.cardRenderer(cardTitle, recipe);
+            this.emit(':responseReady');
+        } else {
+            let speechOutput = this.t('RECIPE_NOT_FOUND_MESSAGE');
+            const repromptSpeech = this.t('RECIPE_NOT_FOUND_REPROMPT');
+            if (itemName) {
+                speechOutput += this.t('RECIPE_NOT_FOUND_WITH_ITEM_NAME', itemName);
+            } else {
+                speechOutput += this.t('RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME');
+            }
+            speechOutput += repromptSpeech;
+
+            this.attributes.speechOutput = speechOutput;
+            this.attributes.repromptSpeech = repromptSpeech;
+
+            this.response.speak(speechOutput).listen(repromptSpeech);
+            this.emit(':responseReady');
+        }
+    },
+    'AMAZON.HelpIntent': function() {
+        this.attributes.speechOutput = this.t('HELP_MESSAGE');
+        this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
+
+        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.emit(':responseReady');
+    },
+    'AMAZON.RepeatIntent': function() {
+        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function() {
+        this.response.speak("Goodbye!");
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function() {
+        this.response.speak("Goodbye!");
+        this.emit(':responseReady');
+    },
+    'SessionEndedRequest': function() {
+        console.log(`Session ended: ${this.event.request.reason}`);
+    },
+    'Unhandled': function() {
+        this.attributes.speechOutput = this.t('HELP_MESSAGE');
+        this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
+        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.emit(':responseReady');
+    },
+};
+
+exports.handler = function(event, context, callback) {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
